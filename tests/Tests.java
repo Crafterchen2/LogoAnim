40e350e54c3c8c78db58e9b0c89493d5e76c67ff
Crafterchen2 <Decker_ben@outlook.de>
2025-05-26 18:12:22 +0200

    Modified Tests

--- tests/Tests.java
+++ tests/Tests.java
@@ -1,10 +1,6 @@
 import com.github.crafterchen2.logoanim.*;
-import com.github.crafterchen2.logoanim.components.AssetSelector;
-import com.github.crafterchen2.logoanim.components.LogoDisplay;
-import com.github.crafterchen2.logoanim.components.MoodSelector;
-import com.github.crafterchen2.logoanim.frames.ClientConnectorFrame;
-import com.github.crafterchen2.logoanim.frames.LogoFrame;
-import com.github.crafterchen2.logoanim.frames.StreamFrame;
+import com.github.crafterchen2.logoanim.components.*;
+import com.github.crafterchen2.logoanim.frames.*;
 
 import javax.swing.*;
 import java.awt.*;
@@ -17,11 +13,80 @@ public class Tests {
 	
 	//Methods {
 	public static void main(String[] args) {
-		clientTest();
+		//regexTest();
+		//addingTest();
+		//managerLayoutTest();
+		serverTest();
+		//clientTest();
+		//clientTest();
+		//clientTest();
+	}
+	
+	private static void regexTest(){
+		String[] strings = new String[] { //In comment = wanted result
+				"123.123.123.123:7777", //["123.123.123.123", "7777"]
+				"hallo.de:7777", //["hallo.de", "7777"]
+				"123.123.123.123:", //["123.123.123.123"]
+				"hallo.de:", //["hallo.de"]
+				"123.123.123.123", //["123.123.123.123"]
+				"hallo.de", //["hallo.de"]
+				"23:23:23:23:23:23:23:23", //["23:23:23:23:23:23:23:23"]
+				"[23:23:23:23:23:23:23:23]:7777", //["23:23:23:23:23:23:23:23", "7777"]
+				"23:23::23:23:23:23", //["23:23::23:23:23:23"]
+				"[23:23::23:23:23:23]:7777", //["23:23::23:23:23:23", "7777"]
+				"[23:23::23:23:23:23]::7777", //["[23:23::23:23:23:23]::7777"]
+				"[23:23::23:23:23:23]::77:e:77", //["[23:23::23:23:23:23]::77:e:77"]
+				"[23:23::23:23:23:23]::77:e77", //["[23:23::23:23:23:23]::77:e77"]
+		};
+		for (int i = 0; i < strings.length; i++) {
+			String[] parts = strings[i].split(":(?=\\d+$)", 2);
+			System.out.println(i + ": " + strings[i] + " => " + Arrays.toString(parts));
+		}
+	}
+	
+	private static void addingTest(){
+		JFrame frame = new JFrame("Adding Test");
+		frame.setSize(300,600);
+		frame.setResizable(false);
+		frame.setLocationRelativeTo(null);
+		frame.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+		JPanel panel = new JPanel(new GridLayout(0,1));
+		JButton adder = new JButton("add");
+		adder.addActionListener(e -> {
+			panel.add(new JButton("Button"));
+			panel.repaint();
+		});
+		panel.add(adder);
+		frame.setContentPane(panel);
+		frame.setVisible(true);
+	}
+	
+	private static void managerLayoutTest(){
+		JFrame frame = new JFrame("managerLayoutTest");
+		frame.setSize(300,600);
+		frame.setResizable(false);
+		frame.setLocationRelativeTo(null);
+		frame.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+		JPanel content = new JPanel(new VerticalListLayout());
+		for (int i = 0; i < 10; i++) {
+			JButton button = new JButton("Button");
+			button.setSize(new Dimension(40 * 6, 40));
+			content.add(button);
+		}
+		frame.setContentPane(content);
+		frame.setVisible(true);
+	}
+	
+	private static void serverTest(){
+		new RemoteManagerFrame(null);
 	}
 	
 	private static void clientTest(){
-		new ClientConnectorFrame(new LogoFrame());
+		LogoFrame logo = new LogoFrame();
+		logo.setScale(20);
+		new ClientConnectorFrame(logo);
+		//new ClientConnectorFrame(null);
+		//new PresetLibraryFrame(logo);
 	}
 	
 	private static void testStream(){
