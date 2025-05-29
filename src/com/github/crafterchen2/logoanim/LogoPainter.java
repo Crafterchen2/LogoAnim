1b4c38b56794ad7e2520e7a6c69ad7a32278dfaa
Crafterchen2 <Decker_ben@outlook.de>
2025-05-29 02:48:05 +0200

    Delegated painting to more lightweight LogoPainter

---
+++ src/com/github/crafterchen2/logoanim/LogoPainter.java
@@ -0,0 +1,67 @@
+package com.github.crafterchen2.logoanim;
+
+import java.awt.*;
+import java.util.HashMap;
+
+//Classes {
+public class LogoPainter implements AssetProvider, MoodProvider {
+	
+	//Fields {
+	private final HashMap<RegionEnum, AssetEnum> assets = HashMap.newHashMap(RegionEnum.values().length);
+	private final HashMap<RegionEnum, MoodEnum> moods = HashMap.newHashMap(RegionEnum.values().length);
+	
+	public boolean blink = false;
+	//} Fields
+	
+	//Constructor {
+	
+	
+	public LogoPainter() {
+		this(null, null);
+	}
+	
+	public LogoPainter(AssetProvider defAssets, MoodProvider defMoods) {
+		setAsset(defAssets);
+		setMood(defMoods);
+	}
+	//} Constructor
+	
+	//Methods {
+	public void paint(Graphics g, int w, int h) {
+		g.setColor(new Color(0, 0, 0));
+		if (w <= 0 && h <= 0) return;
+		g.setClip(0, 0, w, h);
+		g.fillRect(0, 0, w, h);
+		RegionEnum[] regs = RegionEnum.values();
+		for (int i = regs.length - 1; i >= 0; i--) {
+			if (regs[i].type == AssetType.EYE && blink) continue;
+			AssetEnum asset = getAsset(regs[i]);
+			if (asset != null) asset.paint(g, regs[i], getMood(regs[i]));
+		}
+	}
+	//} Methods
+	
+	//Overrides {
+	@Override
+	public MoodEnum getMood(RegionEnum reg) {
+		return moods.get(reg);
+	}
+	
+	@Override
+	public AssetEnum getAsset(RegionEnum reg) {
+		return assets.get(reg);
+	}
+	
+	@Override
+	public void setMood(RegionEnum reg, MoodEnum mood) {
+		moods.put(reg, mood);
+	}
+	
+	@Override
+	public void setAsset(RegionEnum reg, AssetEnum asset) {
+		assets.put(reg, asset);
+	}
+	//} Overrides
+	
+}
+//} Classes
--- src/com/github/crafterchen2/logoanim/components/LogoDisplay.java
+++ src/com/github/crafterchen2/logoanim/components/LogoDisplay.java
@@ -10,10 +10,9 @@ import java.util.HashMap;
 public class LogoDisplay extends JComponent implements AssetProvider, MoodProvider {
 	
 	//Fields {
-	private final HashMap<RegionEnum, AssetEnum> assets = HashMap.newHashMap(RegionEnum.values().length);
-	private final HashMap<RegionEnum, MoodEnum> moods = HashMap.newHashMap(RegionEnum.values().length);
-	
 	public boolean blink = false;
+	
+	private final LogoPainter painter;
 	//} Fields
 	
 	//Constructor {
@@ -24,8 +23,7 @@ public class LogoDisplay extends JComponent implements AssetProvider, MoodProvid
 	}
 	
 	public LogoDisplay(AssetProvider defAssets, MoodProvider defMoods) {
-		setAsset(defAssets);
-		setMood(defMoods);
+		painter = new LogoPainter(defAssets, defMoods);
 		setDoubleBuffered(true);
 	}
 	//} Constructor
@@ -33,38 +31,28 @@ public class LogoDisplay extends JComponent implements AssetProvider, MoodProvid
 	//Overrides {
 	@Override
 	protected void paintComponent(Graphics g) {
-		g.setColor(new Color(0, 0, 0));
-		int w = getWidth();
-		int h = getHeight();
-		if (w <= 0 && h <= 0) return;
-		g.setClip(0, 0, w, h);
-		g.fillRect(0, 0, w, h);
-		RegionEnum[] regs = RegionEnum.values();
-		for (int i = regs.length - 1; i >= 0; i--) {
-			if (regs[i].type == AssetType.EYE && blink) continue;
-			AssetEnum asset = getAsset(regs[i]);
-			if (asset != null) asset.paint(g, regs[i], getMood(regs[i]));
-		}
+		painter.blink = blink;
+		painter.paint(g, getWidth(), getHeight());
 	}
 	
 	@Override
 	public MoodEnum getMood(RegionEnum reg) {
-		return moods.get(reg);
+		return painter.getMood(reg);
 	}
 	
 	@Override
 	public AssetEnum getAsset(RegionEnum reg) {
-		return assets.get(reg);
+		return painter.getAsset(reg);
 	}
 	
 	@Override
 	public void setMood(RegionEnum reg, MoodEnum mood) {
-		moods.put(reg, mood);
+		painter.setMood(reg, mood);
 	}
 	
 	@Override
 	public void setAsset(RegionEnum reg, AssetEnum asset) {
-		assets.put(reg, asset);
+		painter.setAsset(reg, asset);
 	}
 	//} Overrides
 	
